05ce70b86e72cfc37396f893f8c5d1a20ded0bd7
brilik <megabrilik@gmail.com>
2022-10-02 20:30:14 +0800

    Moved business logic to background

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,10 +1,35 @@
 let mode = false,
-    countRequest = 0
+    timeOut
 
 chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.sync.set({mode})
-    chrome.storage.sync.set({countRequest})
     console.group('Background: onInstalled action')
     console.log(`Default mode set to %c${mode}`, `color: green`)
     console.groupEnd()
 });
+
+chrome.runtime.onMessage.addListener(
+    (request, sender, sendResponse) => {
+        let count = 0
+
+        if (request.mode) {
+            timeOut = setInterval(() => {
+                count++
+                fetch(request.locationHref, {method: "GET"})
+                    .then((data) => {
+                        console.warn(data, count)
+                    })
+            }, request.intervalTime)
+        } else {
+            clearInterval(timeOut)
+        }
+
+        console.log(sender.tab ?
+            "from a content script:" + sender.tab.url :
+            "from the extension");
+
+        sendResponse({
+            countRequests: count,
+        });
+    }
+);
--- assets/js/popup.js
+++ assets/js/popup.js
@@ -3,16 +3,6 @@ const modeBtn = document.getElementById('changeMode')
 const intervalLabel = document.getElementById('periodsLabel')
 const periodsHelperText = document.getElementById('periodsHelperText')
 const periodsTime = document.getElementById('periodsTime')
-const debug = async (...e) => {
-    let [tab] = await chrome.tabs.query({active: true, currentWindow: true})
-    await chrome.scripting.executeScript({
-        target: {tabId: tab.id},
-        func: (...f) => {
-            console.log(...f)
-        },
-        args: [...e]
-    })
-}
 
 intervalLabel.textContent = chrome.i18n.getMessage('periodicity')
 periodsHelperText.textContent = chrome.i18n.getMessage('minute')
@@ -27,35 +17,37 @@ chrome.storage.sync.get(['mode'], ({mode}) => {
     }
 })
 
-modeBtn.addEventListener("click", async () => {
-    const [tab] = await chrome.tabs.query({active: true, currentWindow: true})
+modeBtn.addEventListener("click", () => {
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+        let intervalTime = periodsTime.value ? periodsTime.value * 60 * 1000 : 5 * 60 * 1000
 
-    let intervalTime = periodsTime.value ? periodsTime.value * 60 * 1000 : 5 * 60 * 1000
+        chrome.storage.sync.get(['mode'], ({mode}) => {
+            chrome.storage.sync.set({mode: !mode}).then(() => {
+                if (!mode) { // turn off
+                    modeBtn.innerText = chrome.i18n.getMessage('deactivate')
+                    modeBtn.classList.add('active')
+                } else { // turn on
+                    modeBtn.innerText = chrome.i18n.getMessage('activate')
+                    modeBtn.classList.remove('active')
+                }
 
-    chrome.storage.sync.get(['mode'], ({mode}) => {
-        chrome.storage.sync.set({mode: !mode}).then(() => {
-            if (!mode) { // turn off
-                modeBtn.innerText = chrome.i18n.getMessage('deactivate')
-                modeBtn.classList.add('active')
-            } else { // turn on
-                modeBtn.innerText = chrome.i18n.getMessage('activate')
-                modeBtn.classList.remove('active')
-            }
+                chrome.scripting.executeScript({
+                    target: {tabId: tabs[0].id},
+                    func: (data) => {
+                        chrome.runtime.sendMessage(data, (response) => {
+                            console.group('NotLogoutInActiveTab')
+                            console.log(response)
+                            console.groupEnd()
+                        })
+                    },
+                    args: [{
+                        mode: !mode,
+                        tab: tabs[0].id,
+                        intervalTime: intervalTime,
+                        locationHref: window.location.href,
+                    }],
+                })
+            })
         })
-    })
-
-    await chrome.scripting.executeScript({
-        target: {tabId: tab.id},
-        func: sendRequest,
-        args: [intervalTime],
     })
 })
-
-const sendRequest = (intervalTime) => {
-    setInterval(() => {
-        fetch(window.location.href, {method: 'GET'}
-        ).then((data) => {
-            console.warn(data)
-        })
-    }, intervalTime)
-}
